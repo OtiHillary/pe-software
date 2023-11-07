21e9c1d1d61a4575396e21d5ad85d856264d7819
Otonye Hillary Edwin <102995199+OtiEdwin@users.noreply.github.com>
2023-11-07 11:13:30 +0000

    small changes in page

---
+++ app/(admin)/goals/page.tsx
@@ -0,0 +1,22 @@
+export default function Home(){
+    return(
+        <div className="m-6">
+            <div className="goals flex justify-between">
+                <h1 className="text-xl font-bold">Goals</h1>
+
+                <div className="actions flex justify-between">
+                    <div className="grid rounded-md border mx-4">
+
+                    </div>
+                    <div className="border rounded-md list mx-4">
+
+                    </div>
+                    <div className="bg-pes py-3 px-8 rounded-md text-white new ms-8">
+                        Set new Goal
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+    )
+}
