585b9a3e7a41103937efeb38073624e5afe49265
niwdE-JA <dezwhy97@gmail.com>
2024-03-16 10:16:48 +0100

    none existent payload

--- app/components/modals/editgoal.tsx
+++ app/components/modals/editgoal.tsx
@@ -16,7 +16,7 @@ async function changeGoal() {
 
 export default function Editgoal(){
     const isVisible = useSelector( (state: RootState) => state.goal.edit.visible )
-    const data: goal | {} = useSelector( (state: RootState) => state.goal.edit.data.payload )
+    const data: goal | {} = useSelector( (state: RootState) => state.goal.edit.data?.payload )
     // console.log(`your data is${ data }`)
     const dispatch = useDispatch()
 
