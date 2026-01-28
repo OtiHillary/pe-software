f0014cd90d9c8aa99000220bfc25bf489b61c970
Otonye Hillary <otonye.dev@gmail.com>
2026-01-28 06:42:05 +0100

    commit

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -82,18 +82,18 @@ export default function Sidebar({is_sidebar_active, handleSideBar}:
 
    console.log(pathname.split('/'))
 
-   // roles should be : admin, dean, hod, auditor 
+   // roles should be : admin, dean, hod, auditor, super-admin, industrial-engineer, lecturer, employee-w
    const tabs = [
-      { key: 1, name: 'Dashboard', icon: <Home3 />, href: '/dashboard', role_access: ['admin', 'lecturer', 'industrial-engineer', 'hod', 'employee-w', 'auditor'] },
+      { key: 1, name: 'Dashboard', icon: <Home3 />, href: '/dashboard', role_access: ['admin', 'lecturer', 'industrial-engineer', 'hod', 'employee-w', 'auditor', 'super-admin'] },
       { key: 4, name: 'Employee Database', icon: <People />, href: '/em-database', role_access: ['admin', 'hod'] }, 
-      { key: 5, name: 'Goals', icon: <Setting4 />, href: '/goals', role_access: ['admin', 'lecturer', 'industrial-engineer', 'hod', 'employee-w'] }, 
-      { key: 3, name: 'Data Entry', icon: <LucideDatabase />, href: '/data-entry', role_access: ['lecturer', 'industrial-engineer', 'hod', 'employee-w', 'auditor'] }, 
-      { key: 6, name: 'Assessment', icon: <Award />, href: '/assessment', role_access: ['admin'] }, 
-      { key: 7, name: 'Performance Review', icon: <Teacher />, href: '/performance', role_access: ['lecturer', 'industrial-engineer', 'hod', 'employee-w'] }, 
-      { key: 2, name: 'Profile', icon: <ProfileCircle />, href: '/profile', role_access: ['lecturer', 'industrial-engineer', 'hod', 'employee-w', 'auditor'] },
-      { key: 8, name: 'Pricing', icon: <DollarCircle />, href: '/pricing', role_access: ['admin'] },
-      { key: 9, name: 'Maintenance Model', icon: <Setting3 />, href: '/maintenance', role_access: [ 'industrial-engineer', 'admin'] },
-      { key: 10, name: 'Other Models', icon: <Setting2 />, href: '/models', role_access: ['industrial-engineer', 'admin'] }
+      { key: 5, name: 'Goals', icon: <Setting4 />, href: '/goals', role_access: ['admin', 'lecturer', 'industrial-engineer', 'hod', 'employee-w', 'super-admin'] }, 
+      { key: 3, name: 'Data Entry', icon: <LucideDatabase />, href: '/data-entry', role_access: ['lecturer', 'industrial-engineer', 'hod', 'employee-w', 'auditor', 'super-admin'] }, 
+      { key: 6, name: 'Assessment', icon: <Award />, href: '/assessment', role_access: ['admin', 'super-admin'] }, 
+      { key: 7, name: 'Performance Review', icon: <Teacher />, href: '/performance', role_access: ['lecturer', 'industrial-engineer', 'hod', 'employee-w', 'super-admin'] }, 
+      { key: 2, name: 'Profile', icon: <ProfileCircle />, href: '/profile', role_access: ['lecturer', 'industrial-engineer', 'hod', 'employee-w', 'auditor', 'super-admin'] },
+      { key: 8, name: 'Pricing', icon: <DollarCircle />, href: '/pricing', role_access: ['admin', 'super-admin'] },
+      { key: 9, name: 'Maintenance Model', icon: <Setting3 />, href: '/maintenance', role_access: [ 'industrial-engineer', 'admin', 'super-admin'] },
+      { key: 10, name: 'Other Models', icon: <Setting2 />, href: '/models', role_access: ['industrial-engineer', 'admin', 'super-admin'] }
    ]
 
    const allowedTabs = tabs.filter(tab => tab.role_access.includes(user.role));
---
+++ app/login/admin/page.tsx
@@ -0,0 +1,143 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import { useEffect, useState } from "react";
+import { useAuth } from "@/app/components/useAuth";
+
+type formdata = {
+  email: string;
+  password: string;
+};
+
+export default function Home() {
+  const { setRole } = useAuth();
+  const [message, setMessage] = useState({
+    visibility: "invisible",
+    text: "",
+    color: "",
+  });
+  const router = useRouter();
+
+  const schema = Yup.object({
+    email: Yup.string()
+      .email("Invalid email address format")
+      .required("Email is required"),
+
+    password: Yup.string()
+      .min(3, "Password must be 3 characters at minimum")
+      .required("Password is required"),
+  });
+
+  async function login(url: string, data: formdata) {
+    setMessage({ visibility: "visible", text: "loading", color: "green" });
+
+    try {
+      const req = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      let res = await req.json();
+
+      if (res.status == 200) {
+        localStorage.setItem("access_token", res.token);
+
+        setRole(res.role);
+
+        document.cookie = `role=${res.role}; path=/; max-age=86400`;
+
+        router.push("/dashboard");
+      } else if (res.status == 500) {
+        setMessage({
+          visibility: "visible",
+          text: "login failed, check details",
+          color: "red",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem("access_token")) {
+      router.push("/dashboard");
+    }
+  }, [router]);
+
+  return (
+    <main className="w-full flex overflow-hidden relative">
+      {/* message box */}
+      <div
+        style={{ borderColor: message.color }}
+        className={`z-10 bg-white absolute p-6 px-12 shadow-md rounded-md border text-gray-600 font-semibold ${message.visibility} top-3 left-1/2 -translate-x-1/2`}
+      >
+        {message.text}
+      </div>
+
+      {/* illustration */}
+      <div className="illustration bg-pes-gradient w-1/2 h-screen relative flex">
+        <Image
+          src={"/pes.svg"}
+          alt="pes hero image"
+          width={130}
+          height={130}
+          className="z-10 mx-auto my-auto"
+        />
+      </div>
+
+      {/* login form */}
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={schema}
+        onSubmit={(values) => login("/api/login", values)}
+      >
+        {({ isValid, dirty }) => (
+          <Form className="form w-1/2 h-screen flex flex-col p-28 justify-center">
+            <p className="text-4xl text-semibold mb-8">Sign In (Admin)</p>
+
+            <div className="input flex flex-col justify-center mb-4">
+              <label htmlFor="email" className="mb-1">
+                Email Address:
+              </label>
+              <Field
+                className="bg-transparent border border-gray-200 text-gray-700 focus:outline-pes ps-4 py-2 rounded-lg"
+                type="email"
+                name="email"
+                id="email"
+                required
+              />
+            </div>
+
+            <div className="input flex flex-col justify-center mb-4">
+              <label htmlFor="password" className="mb-1">
+                Password:
+              </label>
+              <Field
+                className="bg-transparent border border-gray-200 text-gray-700 focus:outline-pes ps-4 py-2 rounded-lg"
+                type="password"
+                name="password"
+                id="password"
+                required
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="btn bg-pes text-white px-4 py-3 flex justify-center rounded-lg mb-2 
+                         disabled:opacity-50 disabled:cursor-not-allowed"
+              disabled={!(dirty && isValid)}
+            >
+              Sign In
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </main>
+  );
+}
